d26c0e68553cb44103f31b10d73cd5cf3b47bab1
pixelluisxx <pixelluisxx@gmail.com>
2025-09-23 17:39:02 -0500

    C#: Secuencial – promedio (ejemplo 1)

---
+++ 02_EstructurasControl/Secuenciales/CSharp/promedio.cs
@@ -0,0 +1,10 @@
+using System;
+class Program {
+  static void Main() {
+    double a=double.Parse(Console.ReadLine());
+    double b=double.Parse(Console.ReadLine());
+    double c=double.Parse(Console.ReadLine());
+    double d=double.Parse(Console.ReadLine());
+    Console.WriteLine((a+b+c+d)/4.0);
+  }
+}
